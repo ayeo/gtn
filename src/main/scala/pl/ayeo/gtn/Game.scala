69fef22a1470f9b63ff7565ee7331be4d761331d
ayeo <me@ayeo.pl>
2021-03-31 23:29:16 +0200

    Refainement

--- src/main/scala/pl/ayeo/gtn/Game.scala
+++ src/main/scala/pl/ayeo/gtn/Game.scala
@@ -10,6 +10,33 @@ object Game extends App {
 
   case class GameState(userName: String, number: Int, guessNo: Int) {
     def fail(): GameState = new GameState(userName, number, guessNo + 1)
+    def newNumber(number: Int): GameState = new GameState(userName, number, 0)
+
+    def handle(guess: Int) =
+      if (guess == number) manageSuccess(this) else manageFailure(this, guess)
+
+    private def manageSuccess(gameState: GameState): ZIO[Console with Random, IOException, Unit] = {
+      val message: String = gameState.guessNo match {
+        case 0 => "Perfect!"
+        case 1 => "Almost perfect"
+        case 2 => "Nice"
+        case n: Int => s"You have got it in ${n} rounds!"
+      }
+
+      putStrLn(message) *> wantToPlayMore
+    }
+
+    private def manageFailure(gameState: GameState, guess: Int): ZIO[Console with Random, IOException, Unit] = {
+      val message = if (guess < gameState.number) "The number is higher" else "The number is lower"
+      putStrLn(message) *> gameLoop(gameState.fail())
+    }
+
+    private def wantToPlayMore(): ZIO[Console with Random, IOException, Unit] = for {
+        _ <- putStrLn("Want to play more?")
+        _ <- getStrLn.flatMap(f => if (f == "y") ZIO.succeed(f) else ZIO.fail(new IOException("See you soon!")))
+        number <- randomNumber(1, 4)
+        _ <- gameLoop(newNumber(number))
+      } yield ()
   }
 
   object GameState {
@@ -18,40 +45,30 @@ object Game extends App {
 
   def getGuess(): ZIO[Console, IOException, Int] = {
     for {
-      _ <- putStrLn("Gimme your bet bitch..")
+      _ <- putStrLn("What is your guess?")
       n <- getStrLn.map(f => f.toInt)
-      k <- if (n > 100) putStr("Stupid or wat?") *> getGuess() else ZIO.succeed(n)
+      k <- if (n > 100 || n < 1) putStr("Number must be between 1-100") *> getGuess() else ZIO.succeed(n)
     } yield k
   }
 
-  def manageSuccess(gameState: GameState): ZIO[Console with Random, IOException, Unit] = gameState.guessNo match {
-    case 0 => putStrLn(s"You lucky bustard!")
-    case 1 => putStrLn(s"Not bad")
-    case 2 => putStrLn(s"So, so")
-    case n: Int  => putStrLn(s"You lame! It took ${n} rounds!")
-  }
-
-  def manageFailure(gameState: GameState, guess: Int): ZIO[Console with Random, IOException, Unit] = {
-    val message = if (guess < gameState.number) "The number is higher" else "Bit to much"
-    putStrLn(message) *> gameLoop(gameState.fail())
-  }
 
 
-  def gameLoop(gameState: GameState): ZIO[Console with Random, IOException, Unit] = {
-    getGuess.flatMap(guess => {
-      if (guess == gameState.number) manageSuccess(gameState)
-      else manageFailure(gameState, guess)
-    })
-  }
+
+  def gameLoop(gameState: GameState): ZIO[Console with Random, IOException, Unit] =
+    for {
+      guess <- getGuess
+      x <- gameState.handle(guess)
+    } yield x
+
 
   val init = for {
-//    _ <- putStrLn("Hello! What is your name?")
-//    name <- getStrLn
-    number <- randomNumber(1, 100)
+    //    _ <- putStrLn("Hello! What is your name?")
+    //    name <- getStrLn
+    number <- randomNumber(1, 4)
     _ <- gameLoop(GameState.withName("Albatros", number))
   } yield ()
 
   val runtime = Runtime.default
-
-  runtime.unsafeRun(init)
+  runtime.unsafeRun(init.catchAll(t => putStrLn(t.getMessage)))
+  //runtime.unsafeRun(init)
 }
